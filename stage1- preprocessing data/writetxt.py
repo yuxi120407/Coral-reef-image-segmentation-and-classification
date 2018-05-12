9f81ff572504724f1756eb7c2078a6ed088ac7bd
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-05-12 16:44:25 -0400

    Update writetxt.py

--- stage1- preprocessing data/writetxt.py
+++ stage1- preprocessing data/writetxt.py
@@ -36,5 +36,57 @@ with open('output_file.txt', 'w+') as f:
 for filename in glob.glob('*.txt'):
     f = open(filename, 'r')
 
-
+#%%
+import sys,os,msvcrt  #导入的模块与方法
+   
+ def join(in_filenames, out_filename):  
+     out_file = open(out_filename, 'w+')  
+       
+     err_files = []  
+     for file in in_filenames:  
+         try:  
+             in_file = open(file, 'r')  
+             out_file.write(in_file.read())  
+             out_file.write('\n\n')  
+             in_file.close()  
+         except IOError:  
+             print 'error joining', file  
+             err_files.append(file)  
+     out_file.close()  
+     
+     print 'joining completed. %d file(s) missed.' % len(err_files)  
+     
+     print 'output file:', out_filename  
+     
+     if len(err_files) > 0:  #判断
+         print 'missed files:'  
+         print '--------------------------------'  
+         for file in err_files:  
+             print file  
+         print '--------------------------------'  
+ #www.iplaypy.com  
+ if __name__ == '__main__':  
+     print 'scanning...'  
+     in_filenames = []  
+     file_count = 0  
+     
+     for file in os.listdir(sys.path[0]):  
+         if file.lower().endswith('[all].txt'):  
+             os.remove(file)  
+         elif file.lower().endswith('.txt'):  
+             in_filenames.append(file)  
+             file_count = file_count + 1  
+     
+     if len(in_filenames) > 0:  
+         print '--------------------------------'  
+         print '\n'.join(in_filenames)  
+         print '--------------------------------'  
+         print '%d part(s) in total.' % file_count  
+         book_name = raw_input('enter the book name: ')  
+         print 'joining...'  
+         join(in_filenames, book_name + '[ALL].TXT')  
+     else:  
+         print 'nothing found.'  
+     
+     msvcrt.getch() 
       
