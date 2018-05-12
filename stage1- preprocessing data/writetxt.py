6d8bcdcef48e43a36f0cf9a97c7b33fd7af54eb0
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-05-12 16:26:06 -0400

    Update writetxt.py

--- stage1- preprocessing data/writetxt.py
+++ stage1- preprocessing data/writetxt.py
@@ -36,28 +36,5 @@ with open('output_file.txt', 'w+') as f:
 for filename in glob.glob('*.txt'):
     f = open(filename, 'r')
 
-#%%
 
-import os
-import sys
-import glob
-def dirTxtToLargeTxt(dir,outputFileName):
-  '''从dir目录下读入所有的TXT文件,将它们写到outputFileName里去'''
-  #如果dir不是目录返回错误
-  if not os.path.isdir(dir):
-    print "传入的参数有错%s不是一个目录" %dir
-    return False
-  #list all txt files in dir
-  outputFile = open(outputFileName,"a")
-  for txtFile in glob.glob(os.path.join(dir,"*.txt")):
-    print txtFile
-    inputFile = open(txtFile,"rb")
-    for line in inputFile:
-      outputFile.write(line)
-  return True
-if __name__ =="__main__":
-  if len(sys.argv) < 3:
-    print "Usage:%s dir outputFileName" %sys.argv[0]
-    sys.exit()
-  dirTxtToLargeTxt(sys.argv[1],sys.argv[2])
       
