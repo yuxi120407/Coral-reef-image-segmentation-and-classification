ab18a0b019a2a78c4a93a3540a1aeaa251b2f45e
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-05-15 17:06:19 -0400

    Update crop_image_cnn.py

--- stage2-trainining with CNN in crop image/crop_image_cnn.py
+++ stage2-trainining with CNN in crop image/crop_image_cnn.py
@@ -80,7 +80,7 @@ model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
-
+#%%
 
 
 # initiate RMSprop optimizer
@@ -135,3 +135,11 @@ else:
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+#%%
+class_names = ['Agalg','DCP','ROC','TWS','CCA','Ana','others']
+Y_pred = model.predict(x_test,verbose=2)
+y_pred = np.argmax(Y_pred,axis=1)
+for ix in range(7):
+        print (ix, confusion_matrix(np.argmax(y_test,axis=1), y_pred)[ix].sum())
+print (confusion_matrix(np.argmax(y_test,axis=1), y_pred))    
+plot_confusion_matrix(confusion_matrix(np.argmax(y_test,axis=1), y_pred), classes=class_names)
