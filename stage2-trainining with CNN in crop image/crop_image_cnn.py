ffb4f4be2671511f31a83270fc5956b61bbe7e8a
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-05-15 15:17:41 -0400

    Update crop_image_cnn.py

--- stage2-trainining with CNN in crop image/crop_image_cnn.py
+++ stage2-trainining with CNN in crop image/crop_image_cnn.py
@@ -55,21 +55,7 @@ print(x_test.shape[0], 'test samples')
 y_train = keras.utils.to_categorical(y_train_vector, num_classes)
 y_test = keras.utils.to_categorical(y_test_vector, num_classes)
 
-#model = Sequential()
-#model.add(Conv2D(32, kernel_size=(3, 3),
-#                 activation='relu',
-#                 input_shape=input_shape))
-#model.add(Conv2D(64, (3, 3), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
-#model.add(Flatten())
-#model.add(Dense(128, activation='relu'))
-#model.add(Dropout(0.5))
-#model.add(Dense(num_classes, activation='softmax'))
-#
-#model.compile(loss=keras.losses.categorical_crossentropy,
-#              optimizer=keras.optimizers.Adadelta(),
-#              metrics=['accuracy'])
+
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same',
                  input_shape=x_train.shape[1:]))
