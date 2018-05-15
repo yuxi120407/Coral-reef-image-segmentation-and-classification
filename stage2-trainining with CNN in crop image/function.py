867df67f3cb43b7665246982d276fcf7a3db21a2
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-05-15 17:45:50 -0400

    Update function.py

--- stage2-trainining with CNN in crop image/function.py
+++ stage2-trainining with CNN in crop image/function.py
@@ -110,24 +110,28 @@ def newimagedata(crop_length,crop_width):
     return final_data,final_label
 #%%
 def create_plots(history):
-
+    fig = plt.figure(figsize=(5,10))
+    ax = fig.add_subplot(*[2,1,1])
     plt.plot(history.history['acc'])
     plt.plot(history.history['val_acc'])
-    plt.title('accuracy of CNN')
+    ax.set_title('accuracy of CNN')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     #plt.savefig('accuracy.png')
     #plt.clf()
-
+    #plt.show()
+    
+    ax = fig.add_subplot(*[2,1,2])
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
-    plt.title('margin_loss of CNN')
+    plt.title('loss of CNN')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
     #plt.savefig('loss.png')
     #plt.clf()
+    plt.show()
     
 def plot_confusion_matrix(confusionmatrix, classes,
                           normalize=False,
