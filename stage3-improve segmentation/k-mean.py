3810f2c8f72adfc9dacb67d17ef2a6d61130558b
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-06-02 16:36:05 -0400

    Update k-mean.py

--- stage3-improve segmentation/k-mean.py
+++ stage3-improve segmentation/k-mean.py
@@ -197,7 +197,66 @@ for txt_name in raw_txt_files:
         label = segments_slic[l_y,l_x]
         generate_sample(original_label,label,10,txt_name)
      txt_file.close()
-
+#%%
+def drew_label_points(image_name):
+    #image_name = str()
+    path_image = str('./2012image/')+image_name+str('.jpg')
+    path_txt = str('./2012new - No_augmentation/')+image_name+str('.txt')
+    print("find points by using k-means... ")
+    img = imread(path_image)
+    img = img_as_float(img)
+    segments_slic = slic(img, n_segments=50, compactness=15, sigma=1)
+    print("k-means done")
+    print("generate 10 points for each label...")
+    count_points = 50
+    txtfile = open(path_txt)
+    lines = txtfile.readlines()[2:52]
+    for i in range(count_points):
+        line_piece = lines[i]
+        list_element = line_piece.split(',')
+        l_x = int(list_element[0])
+        l_y = int(list_element[1])
+        original_label = int(list_element[2])
+        label = segments_slic[l_y,l_x]
+        generate_sample(segments_slic,original_label,label,10,path_txt)
+    txtfile.close()
+    print("geneating done")
+    geneate_count_points = 500
+    txtfile = open(path_txt)
+    all_lines = txtfile.readlines()[2:]
+    lines = all_lines[0:50]
+    generate_lines = all_lines[50:]
+    fig, ax = plt.subplots(1, 1, figsize=(10,10), sharex=True, sharey=True)
+    ax.imshow(mark_boundaries(img, segments_slic,color=(1,1,0)))
+    for i in range(count_points):
+        line_piece = lines[i]
+        list_element = line_piece.split(',')
+        l_x = int(list_element[0])
+        l_y = int(list_element[1])
+        original_label = int(list_element[2])
+        mark_point(l_x,l_y,original_label)
+    plt.savefig('./2012-test-image/'+image_name+'original_points.png',bbox_inches='tight')
+    for j in range(geneate_count_points):
+        line_piece = generate_lines[j]
+        list_element = line_piece.split(',')
+        l_x = int(list_element[0])
+        l_y = int(list_element[1])
+        original_label = int(list_element[2])
+        mark_generate_point(l_x,l_y,original_label)   
+    plt.savefig('./2012-test-image/'+image_name+'generate_points.png',bbox_inches='tight')
+#%%
+image_count = 120
+count = 1
+txt_name = raw_txt_files = glob.glob("./2012new/*.txt")
+for name in txt_name:
+    image_name = name.split("\\")[1]
+    image_name = image_name.split(".")[0]
+    print("Image {0} is processing ".format(count))
+    start_time = time.time()
+    drew_label_points(image_name)
+    #print("Image {0} finished ".format(count))
+    print(("Image %d finished in %f seconds ---" % (count,(time.time()-start_time))))
+    count = count+1
 
 
 
