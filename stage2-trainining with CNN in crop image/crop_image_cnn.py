d691a3a791b495acf7154fe04b753d4f94d21a5a
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-06-06 17:59:30 -0400

    Update crop_image_cnn.py

--- stage2-trainining with CNN in crop image/crop_image_cnn.py
+++ stage2-trainining with CNN in crop image/crop_image_cnn.py
@@ -17,22 +17,28 @@ from keras import backend as K
 from function import newtxt,newimagedata,create_plots,plot_confusion_matrix
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report, confusion_matrix
+import numpy as np 
+import os
 
 batch_size = 128
-num_classes = 8
-epochs = 50
+num_classes = 6
+epochs = 10
+save_dir = os.path.join(os.getcwd(), 'saved_models')
+model_name = '2012images_trained_augmentation-6.6.h5'
 data_augmentation = False
 
 # input crop image dimensions
-img_rows, img_cols = 30, 30
+img_rows, img_cols = 30,30
 
 # the data, split between train and test sets
-crop_image,label = newimagedata(img_rows, img_cols)
-x_train = crop_image[0:5000]
-x_test = crop_image[5000:]
+#crop_image,label = newimagedata(img_rows, img_cols)
+crop_image = final_data
+label = final_label
+x_train = crop_image[0:60000]
+x_test = crop_image[60000:]
 
-y_train_vector = label[0:5000]
-y_test_vector = label[5000:]
+y_train_vector = label[0:60000]
+y_test_vector = label[60000:]
 #%%
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
@@ -54,38 +60,47 @@ print(x_test.shape[0], 'test samples')
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train_vector, num_classes)
 y_test = keras.utils.to_categorical(y_test_vector, num_classes)
-
-
-model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape=x_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-
-
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
 #%%
+def cnn_model():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=x_train.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))       
+    
+    
+    model.add(Conv2D(128, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(128, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25)) 
+    
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes))
+    model.add(Activation('softmax'))
+    return model
 
-
+#%%
+model = cnn_model()
+model.summary()
+#%%
 # initiate RMSprop optimizer
-#opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-opt = keras.optimizers.adma()
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+#opt = keras.optimizers.Adam()
 # Let's train the model using RMSprop
 model.compile(loss='categorical_crossentropy',
               optimizer=opt,
@@ -93,7 +108,7 @@ model.compile(loss='categorical_crossentropy',
 
 if not data_augmentation:
     print('Not using data augmentation.')
-    model.fit(x_train, y_train,
+    history =model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
               validation_data=(x_test, y_test),
@@ -126,20 +141,70 @@ else:
                         epochs=epochs,
                         validation_data=(x_test, y_test)
                         )
-
+#%%
 #model.fit(x_train, y_train,
 #          batch_size=batch_size,
 #          epochs=epochs,
 #          verbose=1,
 #          validation_data=(x_test, y_test))
+
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+model_path = os.path.join(save_dir, model_name)
+model.save(model_path)
+print('Saved trained model at %s ' % model_path)
+
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 #%%
-class_names = ['Agalg','DCP','ROC','TWS','CCA','Ana','others']
-Y_pred = model.predict(x_test,verbose=2)
-y_pred = np.argmax(Y_pred,axis=1)
-for ix in range(7):
-        print (ix, confusion_matrix(np.argmax(y_test,axis=1), y_pred)[ix].sum())
-print (confusion_matrix(np.argmax(y_test,axis=1), y_pred))    
-plot_confusion_matrix(confusion_matrix(np.argmax(y_test,axis=1), y_pred), classes=class_names)
+class_names = ['Coral','DCP','Rock','RA','GA','others']
+def plot_conf_matrix(test):
+    if not test: 
+        Y_pred = model.predict(x_train,verbose=2)
+        y_pred = np.argmax(Y_pred,axis=1)
+        for ix in range(6):
+            print (ix, confusion_matrix(np.argmax(y_train,axis=1), y_pred)[ix].sum())
+        print (confusion_matrix(np.argmax(y_train,axis=1), y_pred))    
+        plot_confusion_matrix(confusion_matrix(np.argmax(y_train,axis=1), y_pred), classes=class_names)
+    else:
+        
+        Y_pred = model.predict(x_test,verbose=2)
+        y_pred = np.argmax(Y_pred,axis=1)
+        for ix in range(6):
+            print (ix, confusion_matrix(np.argmax(y_test,axis=1), y_pred)[ix].sum())
+        print (confusion_matrix(np.argmax(y_test,axis=1), y_pred))    
+        plot_confusion_matrix(confusion_matrix(np.argmax(y_test,axis=1), y_pred), classes=class_names)
+#%%
+
+plot_conf_matrix(True)
+#%%
+create_plots(history)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
