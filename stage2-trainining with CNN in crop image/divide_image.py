70cd6ea91996d03609a8f235d772a16c8233ee6c
Xi Yu <34804271+yuxi120407@users.noreply.github.com>
2018-06-18 17:16:16 -0400

    Update divide_image.py

--- stage2-trainining with CNN in crop image/divide_image.py
+++ stage2-trainining with CNN in crop image/divide_image.py
@@ -13,26 +13,41 @@ from function import cnn_model
 import glob
 import time
 
-#grey_levels = 256
-# Generate a test image
-#test_image = np.random.randint(0,grey_levels, size=(11,11))
-#2012images_data_augmentation_trained_model-50epoch
+#%%find the precent areas of coral
+def percent_coral(testimage):
+    count = 0
+    #blue = np.array([0,0,255], dtype=np.uint8)
+    test = np.uint8(testimage)
+    pixel = np.zeros([1,1,3],dtype = np.uint8)
+    for r in range(0,test.shape[0]):
+        for c in range(0,test.shape[1]):
+            pixel = test[r,c,:]
+            #if(np.all(pixel == blue)):
+            if((pixel[0]==0)and(pixel[1]==0)):
+                count = count +1
+    all_pixel = test.shape[0]*test.shape[1]
+    coral_percent = (count/all_pixel)*100 
+    coral_percent = round(coral_percent,2)
+    return coral_percent
+    
+
 #%%load model and weight
 model = cnn_model()
 model.summary()
-model.load_weights('./saved_models/2012images_trained_model.h5')
+#model.load_weights('CNN_weights-08.h5')
+model.load_weights('./saved_models/2012images_trained_augmentation-6.6.h5')
 #model.load_weights('./saved_models/2012images_trained_model.h5')
 #model.load_weights('./saved_models/cifar10_ResNet14v1_model.007.h5')
 #model.load_weights('./saved_models/2012images_data_augmentation_trained_model-50epoch.h5')
 #%%
 windowsize_r = 30
 windowsize_c = 30
-raw_image_files = glob.glob("./2012-image/*.jpg")
+raw_image_files = glob.glob("./2013-image/*.jpg")
 count = 0
 for name in raw_image_files:
     name_piece = name.split("\\")[1]
     name_image = name_piece.split(".")[0]
-    path_image = './2012-image/'+str(name_image)+'.jpg'
+    path_image = './2013-image/'+str(name_image)+'.jpg'
     testimage = Image.open(path_image)
     test_image = imread(path_image)
     start_time = time.time()
@@ -55,20 +70,22 @@ for name in raw_image_files:
             else:#others---yellow
                 testimage.paste((255,255,0),[c,r,c+windowsize_c,r+windowsize_r])
     count = count+1
-    testimage.save('./2012-image/'+str(name_image)+'result.jpg')
+    percent = percent_coral(testimage)
+    testimage.save('./2013-image/'+str(name_image)+' result'+str(percent)+'.jpg')
     print(("---image%d finished in %s seconds ---" % (count,(time.time()-start_time))))
-    
+    print("there are {0}% coral in this image".format(percent))
 
 
 
 #%%    
-path_image = '201208172_T-12-58-58_Dive_01_041.jpg'
-testimage = Image.open(path_image)
+path_image = './New folder (2)/201308203-T-16-16-18_005 .jpg'
+
+testimage = Image.new("RGB",(2048,1536),(0,0,0))
 test_image = imread(path_image)
 # Define the window size
 windowsize_r = 30
 windowsize_c = 30
-start_time = time.time()
+
 # Crop out the window and calculate the histogram
 for r in range(0,test_image.shape[0] - windowsize_r+1, 6):
     for c in range(0,test_image.shape[1] - windowsize_c+1, 6):
@@ -87,20 +104,45 @@ for r in range(0,test_image.shape[0] - windowsize_r+1, 6):
         elif(y_pred==4):#Ana---green
             testimage.paste((0,255,0),[c,r,c+windowsize_c,r+windowsize_r])
         else:#others---yellow
-            testimage.paste((255,255,0),[c,r,c+windowsize_c,r+windowsize_r])
-print("--- %s seconds ---" % (time.time()-start_time))
-#%%
-for a in range(0,2018,6):
-    print(a)
-#%%
-def testimage():
-    testimage.paste((255,0,0),[1748,1236,2049,1537])
-    testimage.paste((255,0,0),[0,0,300,300])
-    plt.imshow(testimage)
-    plt.axis('off')
-#%%
+            testimage.paste((255,255,0),[c,r,c+windowsize_c,r+windowsize_r])           
+percent = percent_coral(testimage)
+time_perimage = time.time()-start_time
+time_perimage = round(time_perimage,2)
+print("image finished in {0}s".format(time_perimage))
+print("there are {0}% coral in this image".format(percent))
 plt.imshow(testimage)
 plt.axis('off')
+#%%
+count = 0
+result_image_files = glob.glob("./2012-result/*.jpg")
+for name in result_image_files:
+    testimage = Image.open(name)
+    name_piece = name.split("\\")[1]
+    name_image = name_piece.split(".")[0]
+    percent = percent_coral(testimage)
+    testimage.save('./2012-result/'+str(name_image)+str(percent)+'.jpg')
+    count = count +1
+    print("image {0} finished".format(count))
+     
+#%%
+count = 0
+blue = np.array([0,0,255], dtype=np.uint8)
+test = np.uint8(testimage)
+pixel = np.zeros([1,1,3],dtype = np.uint8)
+for r in range(0,test.shape[0]):
+    for c in range(0,test.shape[1]):
+        pixel = test[r,c,:]
+        #if(np.all(pixel == blue)):
+        if((pixel[0]==0)and(pixel[1]==0)):
+            count = count +1
+all_pixel = test.shape[0]*test.shape[1]
+coral_percent = (count/all_pixel)*100 
+coral_percent = round(coral_percent,2)        
+        
+        
+        
+
+
 
 
 
