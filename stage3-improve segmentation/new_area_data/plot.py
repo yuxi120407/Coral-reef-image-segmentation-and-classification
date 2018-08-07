076ee3946c3bebda2df1ba9207b11f246e22aceb
Xi Yu <yuxi@xatu.edu.cn>
2018-08-07 18:01:58 -0400

    Update plot.py

--- stage3-improve segmentation/new_area_data/plot.py
+++ stage3-improve segmentation/new_area_data/plot.py
@@ -14,30 +14,33 @@ import textwrap
 import glob
 from skimage.io import imread  
 #%%
-path_txt = './2012_new/201208172_T-12-46-15_Dive_01_017.txt'
-path_image = './201208172_T-12-58-58_Dive_01_041.jpg'
-txtfile = open(path_txt)
-firstline = txtfile.readlines()[2:] 
-txtfile.close()
-#%%
-#Load Data
-Data = np.loadtxt('./2012_new/201208172_T-12-46-15_Dive_01_017.txt')
-#SpheresData = np.loadtxt('spheres.txt')
-#SwissrollData = np.loadtxt('swissroll.txt')
-#%%
+txt_name = glob.glob("./2012_new/*.txt")
+data = np.zeros([1,3])
+for name in txt_name: 
+    image_name = name.split("\\")[1]
+    image_name = image_name.split(".")[0]
+    path_txt = str('./2012_new/')+image_name+str('.txt')
+    path_image =  str('./2012image/')+image_name+str('.jpg')
+
+    txtfile = open(path_txt)
+    firstline = txtfile.readlines()[2:] 
+    txtfile.close()
+    image = imread(path_image)
+    for i in range(50):
+        all_data = np.zeros([50,3])
+        test_file = firstline[i].split(",")
+        point_x = int(test_file[0])
+        point_y = int(test_file[1])
+        label = int(test_file[2])
+        all_data[i,:]=image[point_y,point_x]
+    data = np.vstack((data,all_data))
+real_data = data[1:]
+
 #plot results
-fig = plt.figure(figsize=(10,10))
-ax = fig.add_subplot(*[3,1,1], projection= '3d')
-ax.scatter(EllipsoidsData[:,0],EllipsoidsData[:,1],EllipsoidsData[:,2])
-myTitle = 'Elliposids'
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(*[1,1,1], projection= '3d')
+ax.scatter(real_data[:,0],real_data[:,1],real_data[:,2])
+#ax.scatter(0,0,255,color='red')
+myTitle = 'source domain'
 ax.set_title("\n".join(textwrap.wrap(myTitle, 20)))
 
-#ax = fig.add_subplot(*[3,1,2], projection= '3d')
-#ax.scatter(SpheresData[:,0],SpheresData[:,1],SpheresData[:,2])
-#myTitle = 'SpheresData'
-#ax.set_title("\n".join(textwrap.wrap(myTitle, 20)))
-#
-#ax = fig.add_subplot(*[3,1,3], projection= '3d')
-#ax.scatter(SwissrollData[:,0],SwissrollData[:,1],SwissrollData[:,2])
-#myTitle = 'SwissrollData'
-#ax.set_title("\n".join(textwrap.wrap(myTitle, 20)))
