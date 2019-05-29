ab41c5339323e8f71c8067cb3b45536e03ad931d
Xi Yu <yuxi@ufl.edu>
2019-05-29 11:34:32 -0400

    Update gui_python.py

--- GUI_python/gui_python.py
+++ GUI_python/gui_python.py
@@ -122,8 +122,8 @@ def mark_wrong_point(x,y):
      markerfacecolor='black', markersize=6)
    plt.axis('off')
    
-def resize( w_box, h_box, pil_image): #参数是：要适应的窗口宽、高、Image.open后的图片  
-    w, h = pil_image.size #获取图像的原始大小     
+def resize( w_box, h_box, pil_image): 
+    w, h = pil_image.size      
     f1 = 1.0*w_box/w   
     f2 = 1.0*h_box/h      
     factor = min([f1, f2])     
